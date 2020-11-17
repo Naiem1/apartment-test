a09a0b9ab70d30f69068e6a6658cba6dd6a0ae05
naiem <naiem4288@gamil.com>
2020-11-17 14:02:16 +0600

    first commit

---
+++ social-buddy-app/build/precache-manifest.a7823dda53ae315309bad24c033c622c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e208c775595ac4d9e63036e79cf3e840",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a413788d8aa8921cd664",
+    "url": "/static/css/main.e6eb17d3.chunk.css"
+  },
+  {
+    "revision": "951e9d6e728a6b418e94",
+    "url": "/static/js/2.26ba658a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.26ba658a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a413788d8aa8921cd664",
+    "url": "/static/js/main.7bf2f8d6.chunk.js"
+  },
+  {
+    "revision": "f59bc4f4711a0f149a6f",
+    "url": "/static/js/runtime-main.2de0c6c4.js"
+  }
+]);
